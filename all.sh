7806975f39e90f08568a3f40a3cd25cb0a0d27de
Christoph Klein <christoph.t.klein@me.com>
2016-02-11 16:50:00 -0600

    First commit

---
+++ all.sh
@@ -0,0 +1,5 @@
+source deactivate
+conda env remove -n foobar
+conda create -n foobar -y python=3.4
+source activate foobar
+bash install.sh
---
+++ install.sh
@@ -0,0 +1,5 @@
+conda install -y numpy scipy six cython networkx  matplotlib seaborn
+conda install -y ipython-notebook
+conda install -y -c omnia oset packmol mdtraj
+
+pip install -e .
